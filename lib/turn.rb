5446d8193f69dcdceeb5ed82f9db3cef2111ba83
abstract-metric-6683 <rsneal01@gmail.com>
2020-04-09 20:53:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,9 +41,12 @@ def turn(board)
   input = gets.strip
 index = input_to_index(input)
 
-  until valid_move?(board, index)
-  turn(board)
-end
+  if valid_move?(board, index)
+      move(board, index, char = "X")
+    else
+      turn
+    end
+    display_board
 
 move(board, index, char = "X")
 display_board(board)
