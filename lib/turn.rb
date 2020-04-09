fb129b33b4042ccc4c374ee27a661f830720f4c5
abstract-metric-6683 <rsneal01@gmail.com>
2020-04-09 20:44:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,7 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board, index)
-  if index.between?(0, 8) && !position_taken?(board, index)
+   index.between?(0, 8) && !position_taken?(board, index)
     return true
   else
     return false
