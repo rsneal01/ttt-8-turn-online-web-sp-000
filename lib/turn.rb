4132c3df24b4b1397fb3e4369d47f7cb81cc2d9c
abstract-metric-6683 <rsneal01@gmail.com>
2020-04-09 17:53:56 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,51 @@
+def display_board(board)
+  
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+end
+
+def input_to_index(user_input)
+  return user_input.to_i - 1
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    return true
+  else
+    return false
+  end
+end
+
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == ""
+    return false
+  elsif board[index] == nil
+    return false
+  else board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
+
+def move(board, index, char = "X")
+  board[index] = char
+  
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+index = input_to_index(input)
+  until valid_move?(board, index)
+  turn(board)
+end
+move(board, index, char = "X")
+display_board(board)
+end
+
